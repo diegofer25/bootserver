2172a86a28a8e50c0786d902b6b3607e2419ed4a
Diego Lamarão <diego.lamarao92@gmail.com>
2019-11-26 10:53:32 -0300

    auto import routes

--- src/ServerCreator.js
+++ src/ServerCreator.js
@@ -1,28 +1,43 @@
+const path = require('path')
+const chalk = require('chalk')
+const requireDir = require('require-dir');
 const app = require('restify').createServer()
-module.exports = ({ dependencies, routes, beforeStart, afterStart }) => ({
+module.exports = ({ dependencies, beforeStart, afterStart }) => ({
 
   async start () {
     if (beforeStart) {
       await beforeStart({ app, dependencies: dependencies })
     }
 
-    this.listenRoutes()
-    this.listenServer()
-
-    if (afterStart) {
-      await afterStart({ app, dependencies: dependencies })
+    if (this.listenRoutes()) {
+      this.listenServer()
+      if (afterStart) {
+        await afterStart({ app, dependencies: dependencies })
+      }
     }
   },
 
   listenRoutes () {
-    Object.entries(routes).forEach(([ fileName, methods ]) => {
-      const path = fileName === 'Index' ? '/' : `/${fileName.toLowerCase()}`
-      Object.entries(methods).forEach(([ method, callback ]) => {
-        app[method](path, async (...args) => {
-          await callback(dependencies, ...args)
+    const routesPath = path.resolve(__dirname, '../../../src/routes')
+    try {
+      const routes = requireDir(routesPath)
+      Object.entries(routes).forEach(([ fileName, methods ]) => {
+        const path = fileName === 'index' ? '/' : `/${fileName}`
+        Object.entries(methods.default).forEach(([ method, callback ]) => {
+          app[method](path, async (...args) => {
+            await callback(dependencies, ...args)
+          })
         })
       })
-    })
+      return true
+    } catch (err) {
+      console.log(
+        "%s Don't exists the folder %s inside the folder %s",
+        chalk.red("ERROR:"),
+        chalk.bold("routes"),
+        chalk.bold("src"))
+      return false
+    }
   },
 
   listenServer () {
